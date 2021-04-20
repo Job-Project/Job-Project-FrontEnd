beed9774511eddadd60551b4b677350c240500a9
성동명 <sungdm0303@gmail.com>
2021-04-20 09:53:13 +0900

    main ui

---
+++ job-project/src/components/board/main_board/MainBoard.js
@@ -0,0 +1,55 @@
+import * as M from '../../../assets/style/board/main_board/MainBoardStyle'
+
+const MainBoard = () => {
+    return (
+        <M.Background>
+            <M.MainCont>
+                <M.MainTitle>인기 게시글</M.MainTitle>
+                <M.BoardsCont>
+                    <M.BoardCont>
+                        <M.BoardName>Mentee's Board</M.BoardName>
+                        <M.BoardBox>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                        </M.BoardBox>
+                    </M.BoardCont>
+                    <M.BoardCont>
+                        <M.BoardName>Worker's Board</M.BoardName>
+                        <M.BoardBox>
+                        <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                            <M.BoardList>
+                                <M.BoardText>면접 관련 질문이 있습니다</M.BoardText>
+                            </M.BoardList>
+                        </M.BoardBox>
+                    </M.BoardCont>
+                </M.BoardsCont>
+            </M.MainCont>
+        </M.Background>
+    )
+}
+
+export default MainBoard;

85c7cd603567d333270eda048639ce37098fa702
성동명 <sungdm0303@gmail.com>
2021-04-21 10:16:49 +0900

    set font

--- job-project/src/App.js
+++ job-project/src/App.js
@@ -1,8 +1,22 @@
+import Nav from './components/global/Nav'
+import Chat from './components/global/Chat'
+import WorkerBoard from './components/board/worker_board/WorkerBoard';
+import styled, { createGlobalStyle } from "styled-components";
 
+    const GlobalStyle = createGlobalStyle`
+      body {
+        font-family: 'NanumSquare', sans-serif;
+      }
+    `;
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <GlobalStyle></GlobalStyle>
+      <Nav></Nav>
+      <WorkerBoard></WorkerBoard>
+      <Chat></Chat>
+    </div>
   );
 }
 

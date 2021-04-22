3b8e15ac3601910dbab3698541d734c1f4089b04
성동명 <sungdm0303@gmail.com>
2021-04-22 19:53:44 +0900

    edit setting

--- job-project/src/App.js
+++ job-project/src/App.js
@@ -1,7 +1,11 @@
-import Nav from './components/global/Nav'
-import Chat from './components/global/Chat'
-import WorkerBoard from './components/board/worker_board/WorkerBoard';
 import styled, { createGlobalStyle } from "styled-components";
+import Nav from "./components/global/Nav";
+import Chat from "./components/global/Chat";
+import MenteeMyPage from './components/user_page/my_page/mentee/MenteeMyPage'
+import MentorMyPage from './components/user_page/my_page/mentor/MentorMyPage'
+import MenteeUserPage from './components/user_page/other_page/MenteeUserPage'
+import { UserProfile } from "./assets/style/user_page/my_page/MenteeMyPageStyle";
+import MentorUserPage from "./components/user_page/other_page/MentorUserPage";
 
     const GlobalStyle = createGlobalStyle`
       body {
@@ -13,9 +17,12 @@ function App() {
   return (
     <div>
       <GlobalStyle></GlobalStyle>
-      <Nav></Nav>
-      <WorkerBoard></WorkerBoard>
-      <Chat></Chat>
+      {/* <Nav></Nav>
+      <MentorUserPage></MentorUserPage> */}
+      {/* <MenteeUserPage></MenteeUserPage> */}
+      {/* <Chat></Chat> */}
+      {/* <MentorMyPage></MentorMyPage> */}
+      {/* <MenteeMyPage></MenteeMyPage> */}
     </div>
   );
 }

7388dc8f0ad5dccb9e246261b5f7278d503dd37c
성동명 <sungdm0303@gmail.com>
2021-04-22 22:06:28 +0900

    edit setting

--- job-project/src/App.js
+++ job-project/src/App.js
@@ -1,11 +1,6 @@
 import styled, { createGlobalStyle } from "styled-components";
 import Nav from "./components/global/Nav";
 import Chat from "./components/global/Chat";
-import MenteeMyPage from './components/user_page/my_page/mentee/MenteeMyPage'
-import MentorMyPage from './components/user_page/my_page/mentor/MentorMyPage'
-import MenteeUserPage from './components/user_page/other_page/MenteeUserPage'
-import { UserProfile } from "./assets/style/user_page/my_page/MenteeMyPageStyle";
-import MentorUserPage from "./components/user_page/other_page/MentorUserPage";
 
     const GlobalStyle = createGlobalStyle`
       body {

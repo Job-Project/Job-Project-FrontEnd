4169dde41d1a2fec6ca63909f532bcf910c04edf
성동명 <sungdm0303@gmail.com>
2021-04-27 20:15:59 +0900

    edit setting

--- job-project/src/App.js
+++ job-project/src/App.js
@@ -1,25 +1,28 @@
 import styled, { createGlobalStyle } from "styled-components";
 import Nav from "./components/global/Nav";
 import Chat from "./components/global/Chat";
+// import Posting from "./components/posting/Posting";
+// import Post from "./components/posting/Post"
 
     const GlobalStyle = createGlobalStyle`
       body {
         font-family: 'NanumSquare', sans-serif;
         outline: none;
       }
+      ::selection {
+        background: #DDDDDD;
+        color: #000;
+      }
     `;
 
 function App() {
   return (
-    <div>
+    <>
       <GlobalStyle></GlobalStyle>
-      {/* <Nav></Nav>
-      <MentorUserPage></MentorUserPage> */}
-      {/* <MenteeUserPage></MenteeUserPage> */}
-      {/* <Chat></Chat> */}
-      {/* <MentorMyPage></MentorMyPage> */}
-      {/* <MenteeMyPage></MenteeMyPage> */}
-    </div>
+      <Nav></Nav>
+      {/* <Posting></Posting> */}
+      {/* <Post></Post> */}
+    </>
   );
 }
 

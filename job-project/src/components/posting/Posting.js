d697fbd967163d22db02c763d1a8e1d16c9ee1ca
성동명 <sungdm0303@gmail.com>
2021-06-07 09:22:15 +0900

    posting function

--- job-project/src/components/posting/Posting.js
+++ job-project/src/components/posting/Posting.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import axios from 'axios'
 import * as P from '../../assets/style/posting/PostingStyle'
 import profile_img from '../../assets/images/common/profile.svg'
+import {useHistory} from 'react-router-dom'
 
 const Posting = () => {
+    const history = useHistory()
     const getFileName = () => {
         let fileName = document.getElementById('add_file').value.split('\\').pop();
         document.getElementById("upload-name").value = fileName;
     }
 
+    const submitPost = () => {
+        const title = document.getElementById('postTitle').value
+        const content = document.getElementById('postText').value
+        const cookieArr = document.cookie.split(';')
+        let tokenData = []
+        cookieArr.forEach(e => {
+            const token = e.split('=')
+            tokenData.push(token[1])
+        })
+        const headers = {
+            'Authorization': "Bearer " + tokenData[0]
+        }
+        console.log(headers)
+        const category = 'MENTEE'
+        const data = {title, content, category}
+        console.log(data)
+        axios.post('http://10.156.145.209:8000/post', data, {headers})
+        .then((res) => {
+            console.log(res)
+            history.push({
+                pathname: '/log/Post'
+            })
+        })
+        .catch((res) => {
+            console.log(res)
+        })
+    }
     return(
         <P.Background>
             <P.PostingSection>
@@ -15,25 +45,25 @@ const Posting = () => {
                     <P.ArticleBox>
                         <P.PostingTitle>
                             <P.ProfileImg src={profile_img}></P.ProfileImg>
-                            <P.PostTitle placeholder="제목을 입력하세요"></P.PostTitle>
+                            <P.PostTitle id='postTitle' placeholder="제목을 입력하세요"></P.PostTitle>
                         </P.PostingTitle>
                         <P.PostingCont>
                             <P.PostingInputBox>
-                                <P.PostingInput placeholder="질문 내용을 입력하세요"></P.PostingInput>
+                                <P.PostingInput id='postText' placeholder="질문 내용을 입력하세요"></P.PostingInput>
                             </P.PostingInputBox>
                             <P.PostingAddFileBox>
                                 <P.PostingFile>
-                                    <P.PostingFileName id="upload-name" value="파일을 추가하세요" disabled="disabled"></P.PostingFileName>
+                                    <P.PostingFileName id="upload-name" disabled="disabled"></P.PostingFileName>
                                 </P.PostingFile>
                                 <P.PostingAddFileBtn>
                                     <P.PAFIcon for="add_file"></P.PAFIcon>
                                     <P.PAFBtnText for="add_file">파일 첨부하기</P.PAFBtnText>
-                                    <P.AddFileBtn id="add_file" onChange={getFileName}></P.AddFileBtn>
+                                    <P.AddFileBtn id="add_file" name="file" onChange={getFileName}></P.AddFileBtn>
                                 </P.PostingAddFileBtn>
                             </P.PostingAddFileBox>
                         </P.PostingCont>
                         <P.PostingBtn>
-                            <P.PostingBtnText>질문 올리기</P.PostingBtnText>
+                            <P.PostingBtnText onClick={submitPost}>질문 올리기</P.PostingBtnText>
                         </P.PostingBtn>
                     </P.ArticleBox>
                 </P.PostingArticle>
